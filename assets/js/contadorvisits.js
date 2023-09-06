0543f45ea5ce15e0e733a050958afeca99c13f33
Alecio L. Medeiros <Alexdesaran@outlook.com>
2023-09-06 10:33:57 -0300

    deploy CSS and JS

--- assets/js/contadorvisits.js
+++ assets/js/contadorvisits.js
@@ -1,16 +1,13 @@
 // Verifique se o contador de visitas está armazenado em localStorage
 let contador = localStorage.getItem('contador-visitas');
-
 if (contador === null) {
   // Se não houver contador, defina-o como 1
-  contador = 1;
+  contador = 1548;
 } else {
   // Se houver contador, incremente-o
   contador = parseInt(contador) + 1;
 }
-
 // Atualize o valor do contador no HTML
-document.getElementById('contador-visitas').textContent = contador + ' visitas';
-
+document.getElementById('contador-visitas').textContent = contador ;
 // Armazene o novo valor do contador em localStorage
 localStorage.setItem('contador-visitas', contador);
